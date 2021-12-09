e2dc500aa5c601385cbc63a34e61c9ee9d7495b9
anoop <anoopkr5573@gmail.com>
2021-12-09 21:36:57 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {RouterModule,Routes}from '@angular/router'
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+const mypath:Routes=[
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+
+    
+]
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(mypath)
   ],
   providers: [],
   bootstrap: [AppComponent]
